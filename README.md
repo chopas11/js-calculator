e2f6f93695685439e8ca73712923b6bf73108bbc
Chopas <alexander.nikiforov.02@gmail.com>
2021-09-18 23:46:40 +0300

    Addition to Matrix Class

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# webpack-example
-Webpack 5 Template with Babel7, Typescript, CSS Preprocessors Support
+<p align="center"><a href="https://github.com/chopas11/js-calculator" target="_blank" rel="noopener noreferrer"><img width="100" src="https://raw.githubusercontent.com/chopas11/js-calculator/master/src/images/favicon.ico" alt="Vue logo"></a></p>
+
+<p align="center">
+    <a href="https://nodejs.org/"><img src="https://img.shields.io/node/v/webpack"/></a>
+    <a href=""><img src="https://img.shields.io/npm/v/webpack"/></a>
+    <a href=""><img src="https://img.shields.io/github/repo-size/chopas11/webpack-example"/></a>
+
+
+</p>
+
+<h2 align="center">Javascript Calculator</h2>
+
+This repository includes Functions and Classes for working with mathematical objects and calculation.
+
+
+## Installation
+
+Install all nodejs dependencies:
+
+    $ npm i
+
+To start project in development mode, run:
+
+    $ npm run serve
+
+To build project for production, run:
+
+    $ npm run build
--- src/js/models/Matrix.js
+++ src/js/models/Matrix.js
@@ -10,9 +10,19 @@ export default class Matrix {
       this.columns = 0;
     }
   }
+  // Getters
+  get isSquare() {
+    return (this.rows === this.columns) ? true : false;
 
+  }
+
+  get rank() {
+    return Math.min(this.rows, this.columns);
+  }
+
+  // Instance Methods
   addRow(row) {
-    if (this.rows == 0) this.columns = row.length;
+    if (this.rows === 0) this.columns = row.length;
     row.length = this.columns;
     // Еще нужно дописать изменение empty на ноль в случае недостающей длины
     this.body.push(row);
@@ -23,32 +33,58 @@ export default class Matrix {
     for (let i = rowPosition; i < this.rows - 1; i++)
       this.body[i] = this.body[i + 1];
     this.body.pop();
-    if (this.rows == 1) this.columns = 0;
+    if (this.rows === 1) this.columns = 0;
     this.rows--;
   }
 
   addColumn(column) {
-    let range = (this.rows == 0) ? column.length : this.rows;
+    let range = (this.rows === 0) ? column.length : this.rows;
     // Еще нужно дописать изменение empty на ноль в случае недостающей длины
     for (let i = 0; i < range; i++) {
-      if (this.rows == 0) this.body[i] = [];
+      if (this.rows === 0) this.body[i] = [];
       this.body[i][this.columns] = column[i];
     }
-    if (this.rows == 0) this.rows = column.length;
+    if (this.rows === 0) this.rows = column.length;
     this.columns++;
   }
 
   removeColumn(columnPosition) {
-    // col - Столбец, row - строка
     for (let col = columnPosition; col < this.columns; col++) {
       for (let row = 0; row < this.rows; row++)
-        (col != this.columns - 1) ? this.body[row][col] = this.body[row][col+1] : this.body[row].pop();
+        (col !== this.columns - 1) ? this.body[row][col] = this.body[row][col+1] : this.body[row].pop();
     }
-    if (this.columns == 1) {
+    if (this.columns === 1) {
       this.rows = 0;
       this.body = [];
     }
     this.columns--;
+  }
+
+  getMainDiagonal() {
+    if (this.rows !== this.columns) return [];
+    let mainDiagonal = [];
+    for (let row = 0; row < this.rows; row++)
+      mainDiagonal.push(this.body[row][row]);
+    return mainDiagonal;
+  }
+
+  getSideDiagonal() {
+    if (this.rows !== this.columns) return [];
+    let sideDiagonal = [];
+    for (let row = 0; row < this.rows; row++)
+      sideDiagonal.push(this.body[row][this.columns-row-1]);
+    return sideDiagonal;
+  }
+
+  getRowWithNum(row) {
+    return this.body[row];
+  }
+
+  getColumnWithNum(column) {
+    let columnWithNum = [];
+    for (let row = 0; row < this.rows; row++)
+      columnWithNum.push(this.body[row][column]);
+    return columnWithNum;
   }
 
   multiplyByNumber(number) {
@@ -56,8 +92,10 @@ export default class Matrix {
       this.body[i] = this.body[i].map((element) => element * number);
   }
 
-  get rank() {
-    return Math.min(this.rows, this.columns);
+  static multiplyByNumber(matrix, number) {
+    for (let i = 0; i < matrix.length; i++)
+      matrix[i] = matrix[i].map((element) => element * number);
+    return matrix;
   }
 
   transposition() {
@@ -73,76 +111,76 @@ export default class Matrix {
     this.rows = buffer.length;
   }
 
-  get isSquare() {
-    return (this.rows == this.columns) ? true : false;
-
+  static transposition(matrix) {
+    let transpositionMatrix = [];
+    for (let col = 0; col < matrix.length; col++) {
+      transpositionMatrix.push([]);
+      for (let row = 0; row < matrix[0].length; row++)
+        transpositionMatrix[col].push(matrix[row][col]);
+    }
+    return transpositionMatrix;
   }
 
-  static getMinor(matrix, pos) {
+  // Static Methods
+  static getMinor(matrix, rowNum, colNum) {
     let rows = matrix.length;
     let columns = matrix[0].length;
     let minor = [];
-    Object.assign(minor, matrix)
-    minor.shift();
-    for (let col = pos; col < columns; col++) {
+    // Copy array to minor
+    for (let row = 0; row < rows; row++) {
+      minor.push([]);
+      for (let col = 0; col < columns; col++)
+         minor[row].push(matrix[row][col]);
+    }
+    // remove needed row
+    for (let i = rowNum; i < rows - 1; i++)
+      minor[i] = minor[i + 1];
+    minor.pop();
+    // remove needed column
+    for (let col = colNum; col < columns; col++) {
       for (let row = 0; row < rows-1; row++)
-        (col != rows - 1) ? minor[row][col] = minor[row][col+1] : minor[row].pop();
+        (col !== rows - 1) ? minor[row][col] = minor[row][col+1] : minor[row].pop();
     }
-    return matrix;
+
+    return minor;
   }
 
-  static determinant(matrix) {
-    if(matrix.length == 2) {
+  static getDeterminant(matrix) {
+    // If matrix are not square
+    if (matrix.length != matrix[0].length) return NaN;
+    // Base of recursion - matrix 2x2
+    if(matrix.length == 2)
       return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-    }
     let det = 0;
-    let rows = matrix.length;
-    let columns = matrix[0].length;
-
-    for (let i = 0; i < rows; i++) {
-      det += matrix[0][i] * Math.pow(-1, i) * this.determinant([[1, 5], [4, 0]]);
-      // console.log(this.getMinor(matrix, i))
-    }
-
+    for (let i = 0; i < matrix.length; i++)
+      det += matrix[0][i] * Math.pow(-1, i) * this.getDeterminant(this.getMinor(matrix, 0, i));
     return det;
-
   }
 
-  static wer(matrix) {
-    let buffer = [];
-    for (let i = 0; i < 1; i++) {
-      buffer.push(this.getMinor(matrix, i));
+  // static getDodgsonDeterminant(matrix) {
+  //   // Base of recursion - matrix 2x2
+  //   if(matrix.length == 2)
+  //     return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+  //   let minor2x2Matrix = [];
+  //
+  //   // return minor2x2Matrix;
+  //   console.log(minor2x2Matrix)
+  //   return this.getDodgsonDeterminant(minor2x2Matrix);
+  // }
+
+  static getInverseMatrix(matrix) {
+    let minorMatrix = [];
+
+    for (let row = 0; row < matrix.length; row++) {
+      minorMatrix.push([]);
+      for (let col = 0; col < matrix.length; col++)
+        minorMatrix[row][col] = Math.pow(-1, row + col) * this.getDeterminant(this.getMinor(matrix, row, col));
     }
-    return matrix;
+    return this.multiplyByNumber( this.transposition(minorMatrix) , (1 / this.getDeterminant(matrix)) );
   }
-
-  toString() {
-    return JSON.stringify({
-      body: this.body,
-      rows: this.rows,
-    });
-  }
+  // End of Class
 }
-
-// Необходимые методы:
+// Methods in development:
 /* 
-
-+Добавление/Удаление строк
-+Добавление/Удаление столбцов
-+Умножение матрицы на число
-+Транспонирование матрицы(Переворачиваем строки и столбцы)
-+Вычислениие ранга матрицы
-+Проверка на квадратичность матрицы (Сравнение колва строк и столбцов)
-Поиск детерминанта через разложение по первой строке
-Взятие Главных и побочных диагоналей матрицы
-Взятие Строки или столбца с определенным номером
-Вычисление обратной матрицы
-Поиск ФСР
-
-
-Сложение/Вычитание матриц
-Умножение матриц
-
-
-
+Matrix sum and multiplication
 */
